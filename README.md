0d99b5661052b675ef2f0835039fb0a7c30d36ad
vponugoti1 <vivekanandareddy.ponugoti@slu.edu>
2023-03-23 20:49:41 -0500

    final version of square simulation

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-# Drone-Simulator
-Drone simulator prototype using mavlink protocol
+# Drone Simulator Prototype with MAVLink Protocol
+This project showcases a drone simulator prototype that utilizes the MAVLink protocol and the pymavlink Python library to communicate with MAVLink-enabled autopilots. The script offers a series of functions to interact with and control the drone's behavior.
 
-python3 sim_vehicle.py -v ArduPlane --console --map --out 127.0.0.1:14550
+## Functions Overview
+1. is_vehicle_armed(vehicle): Determines if the vehicle is armed, returning True or False accordingly.
+2. set_target_position(vehicle, north, east, alt): Defines the target position in meters relative to the vehicle's current position using North, East, Down (NED) coordinates.
+3. set_drone_speed(vehicle, speed): Adjusts the drone's speed in meters per second.
+4. change_mode(vehicle, mode_id): Modifies the vehicle's mode to GUIDED mode.
+5. arm_vehicle(vehicle): Arms the vehicle with multiple attempts, incorporating a sleep time between each.
+6. disarm_vehicle(vehicle): Disarms the vehicle.
+7. proportional_controller(distance_to_target, max_speed, gain): Implements a simple proportional controller that calculates the desired speed based on the target distance.
+8. fly_square(vehicle, waypoints, start_alt, max_speed, gain, waypoint_tolerance=2, waypoint_timeout=30): Controls the drone to fly in a square pattern using the provided waypoints.
+9. main(): Coordinates the entire process, including establishing a connection to the drone, waiting for a heartbeat message, changing the vehicle's mode, setting the drone speed, arming and disarming the vehicle, flying the square, and closing the connection.
 
-This modified script now sets a target altitude and location for the simulated plane, flies to the target location, and monitors the vehicle's altitude and location. When the vehicle arrives at the target location, the script will disarm the vehicle and close the connection.
+## Command Line Usage
+To use the drone simulator, run the following commands:
+<code>python3 sim_vehicle.py -v ArduPlane --console --map --out 127.0.0.1:14550</code>
+<code>python3 fixed_wing_simulation.py</code>
+
+With this drone simulator prototype, users can explore the potential of the MAVLink protocol and pymavlink library in controlling drones and managing their various functions.
--- fixed_wing_simulation.py
+++ fixed_wing_simulation.py
@@ -7,59 +7,54 @@ def is_vehicle_armed(vehicle):
     armed_status = vehicle.recv_match(type='HEARTBEAT', blocking=True).base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED
     return bool(armed_status)
 
-# Function to set the target position
-def set_target_position(vehicle, lat, lon, alt):
+# Function to set the target position in meters relative to the current position
+def set_target_position(vehicle, north, east, alt):
     vehicle.mav.mission_item_send(
         vehicle.target_system,
         vehicle.target_component,
         0,  # Sequence number
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED,
         mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
         0,  # Current waypoint
         1,  # Autocontinue
         0, 0, 0, 0,
-        lat, lon, alt
+        north, east, -alt  # Use negative altitude for NED frame
     )
 
-# Create a connection to the simulated plane
-connection_string = "udp:127.0.0.1:14550"
-print(f"Connecting to the simulated plane at {connection_string}")
-vehicle = mavutil.mavlink_connection(connection_string)
+# Function to set drone speed
+def set_drone_speed(vehicle, speed):
+    vehicle.mav.command_long_send(
+        vehicle.target_system,
+        vehicle.target_component,
+        mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED,
+        0,
+        0,  # Airspeed type (0 = ground speed, 1 = true airspeed)
+        speed,  # Target speed in m/s
+        -1,  # Throttle (set to -1 to ignore throttle)
+        0, 0, 0, 0  # Unused parameters
+    )
 
-# Wait for a heartbeat message to establish a connection
-print("Waiting for heartbeat...")
-vehicle.wait_heartbeat()
-print("Heartbeat received. Connection established.")
-
-# Read and print telemetry data
-while True:
-    msg = vehicle.recv_match(type='ATTITUDE', blocking=True)
-    print(f"Roll: {msg.roll}, Pitch: {msg.pitch}, Yaw: {msg.yaw}")
-
-    # Change the vehicle mode to GUIDED
+# Function to change the vehicle mode
+def change_mode(vehicle, mode_id):
     print("Changing mode to GUIDED")
-    vehicle.mav.set_mode_send(vehicle.target_system, mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, 4)
+    vehicle.mav.set_mode_send(vehicle.target_system, mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, mode_id)
 
-    # Monitor the mode change
     while True:
         current_mode = vehicle.recv_match(type='HEARTBEAT', blocking=True).custom_mode
-        if current_mode == 4:
+        if current_mode == mode_id:
             print("Mode changed to GUIDED successfully!")
             break
         else:
             print("Mode change failed. Retrying...")
-            vehicle.mav.set_mode_send(vehicle.target_system, mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, 4)
+            vehicle.mav.set_mode_send(vehicle.target_system, mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, mode_id)
             time.sleep(1)
 
-    # Arm the vehicle
+# Function to arm the vehicle
+def arm_vehicle(vehicle):
     arming_attempts = 0
     max_arming_attempts = 15
 
-    while not is_vehicle_armed(vehicle):
-        if arming_attempts >= max_arming_attempts:
-            print("Failed to arm vehicle after multiple attempts. Exiting.")
-            break
-
+    while not is_vehicle_armed(vehicle) and arming_attempts < max_arming_attempts:
         print(f"Arming the vehicle (attempt {arming_attempts + 1})")
         vehicle.mav.command_long_send(
             vehicle.target_system,
@@ -72,45 +67,11 @@ while True:
         arming_attempts += 1
         time.sleep(2)  # Increase the sleep time to give more time for the vehicle to arm
 
-    if is_vehicle_armed(vehicle):
-        print("Vehicle armed successfully!")
-        break  # Exit the loop since the vehicle is armed
-    else:
-        print("Arming failed. Retrying...")
+    return is_vehicle_armed(vehicle)
 
-    # Set target altitude and location
-    target_latitude = 47.397741
-    target_longitude = 8.545593
-    target_altitude = 50 # in meters
-
-    print(f"Setting target location to lat: {target_latitude}, lon: {target_longitude}, alt: {target_altitude}")
-    set_target_position(vehicle, target_latitude, target_longitude, target_altitude)
-
-    # Fly to the target location
-    print("Flying to target location")
-    vehicle.mav.command_long_send(
-        vehicle.target_system,
-        vehicle.target_component,
-        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
-        0,
-        0, 0, 0, 0, target_latitude, target_longitude, target_altitude
-    )
-
-    # Monitor the vehicle's altitude and location
-    while True:
-        altitude_msg = vehicle.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
-        current_altitude = altitude_msg.relative_alt / 1000 # Convert from millimeters to meters
-        current_latitude = altitude_msg.lat / 1e7
-        current_longitude = altitude_msg.lon / 1e7
-        print(f"Current altitude: {current_altitude} m, latitude: {current_latitude}, longitude: {current_longitude}")
-
-        distance_to_target = math.sqrt((current_latitude - target_latitude)**2 + (current_longitude - target_longitude)**2)
-        if distance_to_target < 5 and abs(current_altitude - target_altitude) < 1:
-            print("Arrived at target location!")
-            break
-        time.sleep(1)
-
-    print("Mission completed. Disarming the vehicle.")
+# Function to disarm the vehicle
+def disarm_vehicle(vehicle):
+    print("Disarming the vehicle.")
     vehicle.mav.command_long_send(
         vehicle.target_system,
         vehicle.target_component,
@@ -119,5 +80,118 @@ while True:
         0, 0, 0, 0, 0, 0, 0
     )
 
-    print("Vehicle disarmed. Closing connection.")
-    vehicle.close()
+def proportional_controller(distance_to_target, max_speed, gain):
+    speed = gain * distance_to_target
+    return min(speed, max_speed)
+
+# Function to fly the square
+def fly_square(vehicle, waypoints, start_alt, max_speed, gain, waypoint_tolerance=2, waypoint_timeout=30):
+    # Adjust the altitude of the waypoints so that the starting altitude is start_alt
+    waypoints = [(north, east, start_alt) for north, east, _ in waypoints]
+
+    for waypoint in waypoints:
+        north, east, alt = waypoint
+        print(f"Setting target location to N: {north}, E: {east}, Alt: {alt}")
+        set_target_position(vehicle, north, east, alt)
+
+        # Monitor the vehicle's position
+        start_time = time.time()
+        while True:
+            position_msg = vehicle.recv_match(type='LOCAL_POSITION_NED', blocking=True)
+            current_north = position_msg.x
+            current_east = position_msg.y
+            current_down = -position_msg.z  # Convert to altitude
+            print(f"Current position: N: {current_north}, E: {current_east}, A:{current_down}")
+
+            distance_to_target_horizontal = math.sqrt((current_north - north) ** 2 + (current_east - east) ** 2)
+            distance_to_target_vertical = abs(current_down - alt)
+
+            # Calculate the desired speed based on the distance to the target
+            desired_speed_horizontal = proportional_controller(distance_to_target_horizontal, max_speed, gain)
+            desired_speed_vertical = proportional_controller(distance_to_target_vertical, max_speed, gain)
+
+            # Update the drone speed (use the minimum of horizontal and vertical speeds)
+            desired_speed = min(desired_speed_horizontal, desired_speed_vertical)
+            set_drone_speed(vehicle, desired_speed)
+
+            if distance_to_target_horizontal < waypoint_tolerance and distance_to_target_vertical < waypoint_tolerance:
+                print("Arrived at target location!")
+                break
+
+            # Check if the waypoint timeout has been reached
+            elapsed_time = time.time() - start_time
+            if elapsed_time > waypoint_timeout:
+                print(f"Timeout reached for waypoint: N: {north}, E: {east}, Alt: {alt}. Moving to the next waypoint.")
+                break
+
+            time.sleep(1)
+
+    # Add a waypoint at altitude 0
+    set_target_position(vehicle, 0, 0, 0)
+
+    # Monitor the vehicle's position
+    while True:
+        position_msg = vehicle.recv_match(type='LOCAL_POSITION_NED', blocking=True)
+        current_down = -position_msg.z  # Convert to altitude
+        print(f"Current altitude: {current_down}")
+
+        if abs(current_down) < 1:
+            print("Reached altitude 0!")
+            break
+        time.sleep(1)
+
+# Main function to execute the mission
+def main():
+    # Create a connection to the simulated plane
+    connection_string = "udp:127.0.0.1:14550"
+    print(f"Connecting to the simulated plane at {connection_string}")
+    vehicle = mavutil.mavlink_connection(connection_string)
+
+    # Wait for a heartbeat message to establish a connection
+    print("Waiting for heartbeat...")
+    vehicle.wait_heartbeat()
+    print("Heartbeat received. Connection established.")
+
+    # Define the square's side length (in meters)
+    square_side = 5
+
+    # Define the square's side length (in meters)
+    square_side = 3
+
+    # Define the waypoints for the square at a lower altitude
+    waypoints = [
+        (square_side, 0, 5),
+        (square_side, square_side, 5),
+        (0, square_side, 5),
+        (0, 0, 5)
+    ]
+
+    # Read and print telemetry data
+    msg = vehicle.recv_match(type='ATTITUDE', blocking=True)
+    print(f"Roll: {msg.roll}, Pitch: {msg.pitch}, Yaw: {msg.yaw}")
+
+    # Change the vehicle mode to GUIDED
+    change_mode(vehicle, 4)
+
+    # Set drone speed
+    max_speed = 10  # m/s
+    gain = 0.5  # Proportional gain
+
+    # Arm the vehicle
+    if arm_vehicle(vehicle):
+        print("Vehicle armed successfully!")
+
+        # Fly the square
+        fly_square(vehicle, waypoints, start_alt=5, max_speed=max_speed, gain=gain)
+
+        # Disarm the vehicle
+        disarm_vehicle(vehicle)
+
+        # Close the connection
+        print("Closing connection.")
+        vehicle.close()
+
+        print("Mission completed.")
+
+if __name__ == '__main__':
+    main()
